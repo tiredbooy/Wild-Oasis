97bc621e416a2c59cb00a39a9f2c86adace361ab
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-08-10 11:41:31 +0330

    used styled components library

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,30 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
-const H1 = styled.h1`
-  font-size : 30px;
-  font-weight : 600;
-  background-color: yellow;
-`;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius : 7px;
-  background-color: purple;
-  color : white;
-  margin : 20px;
-  cursor : pointer;
-`
-
-const Input = styled.input`
-  border : 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`
 
 const StyledApp = styled.div`
   background-color: orangered;
-  padding : 20px ;
-
-`
+  padding: 20px;
+`;
 
 function App() {
-
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button>Check in</Button>
-      <Button>Check Out</Button>
-
-      <Input type="number" placeholder="Number of Guests" />
-    </StyledApp>
-  )
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as='h1'>The Wild Oasis</Heading>
+        <Heading as='h2'>Check in and out</Heading>
+        <Button>Check in</Button>
+        <Button>Check Out</Button>
+        <Heading as='h3'>Forms</Heading>
+        <Input type="number" placeholder="Number of Guests" />
+      </StyledApp>
+    </>
+  );
 }
 
-export default App
+export default App;
