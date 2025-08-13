74c3b09b479ac99d8be9bf9d8d2a813c855d00a8
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-08-13 17:34:07 +0330

    installed and used react query for first time

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,20 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter-sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
---
+++ src/services/supabase..js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://onscssytzhjeksslojho.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uc2Nzc3l0emhqZWtzc2xvamhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5OTk0NTYsImV4cCI6MjA3MDU3NTQ1Nn0.u2rgbtn9LF6P1HYZ7php5WN-FgjR40Q_Ago0wmhOh6k";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+export default supabase;
