a219bf4143f604f18bf3ca9bcad9ef9703302056
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-09-06 12:05:24 +0330

    added update user info logic

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createEditCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+import { updateCurrentUser } from "../../services/apiAuth";
+
+function useUpdateUser() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUser, isLoading: isUpdating } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: () => {
+      toast.success("User Account successfully Updated");
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isUpdating, updateUser };
+}
+
+export default useUpdateUser;
