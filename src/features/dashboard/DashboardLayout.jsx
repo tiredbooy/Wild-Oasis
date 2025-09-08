3e304b0617b8e403d1d67b759bf5c4508e313785
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-09-08 11:44:35 +0330

    added recent stays and recent bookings logics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner"
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,21 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading : isLoading1, bookings } = useRecentBookings();
+  const {stays , confirmedStays , isLoading : isLoading2} = useRecentStays()
+
+  if(isLoading1 || isLoading2) return <Spinner />
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>{"Today's Activity"}</div>
+      <div>Chart Stay Durations</div>
+      <div>Chart Sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
